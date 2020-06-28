2df6e18313b1de1a5e16310d2d14733a8fdcd8aa
NurKeinNeid <mralexman3000@gmail.com>
2020-06-28 02:01:27 +0200

    addons: Update op prebuilts from OnePlus5T-user 10 QKQ1.191014.012 2006012146 release-keys
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- config.mk
+++ config.mk
@@ -2,7 +2,8 @@ PRODUCT_PACKAGES += \
     OnePlusCamera \
     OnePlusCameraService \
     oneplus-framework-res \
-    OnePlusGallery
+    OnePlusGallery \
+    vendor.oneplus.camera.CameraHIDL-V1.0-java
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/system/etc/permissions/privapp-permissions-oem.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-oem.xml \
